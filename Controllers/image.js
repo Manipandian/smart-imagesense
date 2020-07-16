aa74a2c55d5b828eec005ff496380ce6b9feb7f1
Manikandan <62287301+Manipandian@users.noreply.github.com>
2020-07-16 19:25:36 +0530

    create env for api key

--- Controllers/image.js
+++ Controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 const handleImageUrl = (req, res) => {
     // Instantiate a new Clarifai app by passing in your API key.
-    const app = new Clarifai.App({apiKey: '4470b3bdbc974e228e698234c68c52ec'});
+    const app = new Clarifai.App({apiKey: process.env.CLARIFAI_KEY});
     app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
     .then(response => res.json(response))
     .catch(err => res.status(400).json('Something went wrong with api call', err));
