3a0f490aaf0db6831678f38a535b2f6caadcda09
Manikandan <62287301+Manipandian@users.noreply.github.com>
2020-07-24 11:27:14 +0530

    minimize request

--- Controllers/image.js
+++ Controllers/image.js
@@ -1,10 +1,18 @@
 const Clarifai = require('clarifai');
 
-const handleImageUrl = (req, res) => {
+const handleImageUrl = (req, res, db) => {
     // Instantiate a new Clarifai app by passing in your API key.
     const app = new Clarifai.App({apiKey: process.env.CLARIFAI_KEY});
+    let currentData;
     app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
-    .then(response => res.json(response))
+    .then(response => {
+        db('users')
+        .where('id', '=', req.body.id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(data => currentData = data[0])
+        res.json({boxData: response, entryCount: currentData});
+    })
     .catch(err => res.status(400).json('Something went wrong with api call', err));
 }
 
@@ -14,7 +22,7 @@ const handleImageEnrty = (req, res, db) => {
     .where('id', '=', id)
     .increment('entries', 1)
     .returning('entries')
-    .then(data => res.json(data[0]))
+    .then(data => res.json(data[0]))    
     .catch(err => res.status(400).json('Unable to get entry'))
 }
 
