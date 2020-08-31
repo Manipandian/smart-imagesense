5418e33ff35a53bd01589f79907070fc61fd731a
Manikandan <62287301+Manipandian@users.noreply.github.com>
2020-08-31 19:11:38 +0530

    change model id for heroku

--- Controllers/image.js
+++ Controllers/image.js
@@ -3,7 +3,7 @@ const Clarifai = require('clarifai');
 const handleImageUrl = (req, res) => {
     // Instantiate a new Clarifai app by passing in your API key.
     const app = new Clarifai.App({apiKey: process.env.CLARIFAI_KEY});
-    let detectType = req.body.type === 'face' ? 'a403429f2ddf4b49b307e318f00e528b' : req.body.type === 'apparel' ? '72c523807f93e18b431676fb9a58e6ad' : 'eeed0b6733a644cea07cf4c60f87ebb7';
+    let detectType = req.body.type === 'face' ? 'c0c0ac362b03416da06ab3fa36fb58e3' : req.body.type === 'apparel' ? '72c523807f93e18b431676fb9a58e6ad' : 'eeed0b6733a644cea07cf4c60f87ebb7';
     app.models.predict( detectType, req.body.input)
     .then(response => res.json(response))
     .catch(err => res.status(400).json('Something went wrong with api call', err));
